686e011ae0aeb941f08ce65d775e1b02a28cab24
Ignacio <ignamosconi@gmail.com>
2026-05-11 12:35:55 -0300

    fixing .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,15 +34,12 @@ import { DelayInterceptor } from './common/interceptors/delay.interceptor';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
+        url: config.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
         synchronize: false,
         migrations: ['dist/migrations/*.js'],
         migrationsRun: true,
+        
       }),
     }),
 
